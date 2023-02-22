1ae9acaf23add59935fe909ca4f16385d2363f65
unknown <javier@km.solutif.co.id>
2023-02-22 11:06:26 +0700

    login fe

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,17 +8,18 @@
     <title>Login</title>
 </head>
 <body>
-<div id="bg"></div>
-<form>
-    <div class="form-field">
-        <input type="email" placeholder="Email" required/>
-    </div>
-    <div class="form-field">
-        <input type="password" placeholder="Password" required/>
-    </div>
-    <div class="form-field">
-        <button class="btn" type="submit">Log in</button>
-    </div>
-</form>
+<div id="bg">
+    <form>
+        <div class="form-field">
+            <input type="email" placeholder="Email" required/>
+        </div>
+        <div class="form-field">
+            <input type="password" placeholder="Password" required/>
+        </div>
+        <div class="form-field">
+            <button class="btn" type="submit">Log in</button>
+        </div>
+    </form>
+</div>
 </body>
 </html>
