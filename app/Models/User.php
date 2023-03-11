2ef80021d4f9864668cd2ef7053254004991ac89
unknown <javier@km.solutif.co.id>
2023-03-11 13:18:44 +0700

    Well Done

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
